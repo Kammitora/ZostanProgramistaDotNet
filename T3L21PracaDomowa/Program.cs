8021dec2748e9ff729adf741c9912189dd00fc22
Kammitora <kammitora@outlook.com>
2023-06-01 23:15:58 +0200

    add solution

---
+++ T3L21PracaDomowa/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3L21PracaDomowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.Write("Podaj imię: ");
+                var name = Console.ReadLine();
+
+                Console.Write("Podaj miejsce urodzenia: ");
+                var city = Console.ReadLine();
+
+                Console.Write("Podaj datę urodzenia (DD.MM.RRRR): ");
+
+                if (!DateTime.TryParse(Console.ReadLine(), out DateTime birthDate))
+                {
+                    Console.WriteLine("Nieprawidłowe dane.");
+                    return;
+                }
+
+                var years = DateTime.Now.DayOfYear < birthDate.DayOfYear ? DateTime.Now.Year - birthDate.Year - 1 : DateTime.Now.Year - birthDate.Year;
+
+                Console.WriteLine($"Cześć {name}! Urodziłeś się w {city} i masz {years} lat.");
+                Console.ReadKey();
+
+        }
+    }
+}
---
+++ T3L22PracaDomowa/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3L22PracaDomowa
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTries = 0;
+            Random random = new Random();
+            var properNumber = random.Next(0, 100);
+
+            while (true)
+            {
+                Console.WriteLine("Wybierz liczbę: ");
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Nieprawidłowe dane!");
+                    continue;
+                }
+
+                if (number < properNumber)
+                {
+                    numberOfTries++;
+                    Console.WriteLine("Za mało, spróbuj jeszcze raz.");
+                    continue;
+                }
+
+                else if (number > properNumber)
+                {
+                    numberOfTries++;
+                    Console.WriteLine("Za dużo, spróbuj jeszcze raz.");
+                    continue;
+                }
+
+                else
+                {
+                    numberOfTries++;
+                    Console.WriteLine($"BRAWO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Udało sie w {numberOfTries} próbach.");
+                    return;
+                }
+            }
+
+        }
+    }
+}
