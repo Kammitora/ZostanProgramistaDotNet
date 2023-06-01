beeefa2f01d4899a7ced5209f842e43284707bdc
Kammitora <kammitora@outlook.com>
2023-06-01 23:48:52 +0200

    PD week 4 L 11

---
+++ T4L11PracaDomowa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4L11PracaDomowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczbę całkowitą a ja powiem Ci czy jest parzysta czy nieparzysta: ");
+            if (!int.TryParse(Console.ReadLine(), out int userNumber))
+            {
+                Console.WriteLine("Nieprawidłowe dane!");
+                return;
+            }
+
+            string result = userNumber % 2 == 0 ? "Parzysta" : "Nieparzysta";
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
