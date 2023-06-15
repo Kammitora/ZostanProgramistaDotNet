42b9ac7d74b7b40c8c7b1d2f49fd5b82859550a1
Kammitora <kammitora@outlook.com>
2023-06-15 23:41:11 +0200

    Homework databases

---
+++ T6L13PracaDomowa/DatabaseCreateScript.sql
@@ -0,0 +1,82 @@
+﻿USE [Enterprise]
+GO
+/****** Object:  Table [dbo].[Clients]    Script Date: 15.06.2023 23:40:38 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Clients](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](50) NOT NULL,
+	[LastName] [varchar](50) NOT NULL,
+	[City] [varchar](50) NOT NULL,
+	[PhoneNumber] [varchar](50) NULL,
+	[Email] [varchar](50) NULL,
+ CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[InvoicePositions]    Script Date: 15.06.2023 23:40:38 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[InvoicePositions](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[InvoiceId] [int] NOT NULL,
+	[ProductId] [int] NOT NULL,
+	[Quantity] [int] NULL,
+ CONSTRAINT [PK_InvoicePositions] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Invoices]    Script Date: 15.06.2023 23:40:38 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Invoices](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Number] [varchar](50) NOT NULL,
+	[ClientNumber] [int] NOT NULL,
+	[CreatedDate] [date] NULL,
+ CONSTRAINT [PK_Invoices] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Products]    Script Date: 15.06.2023 23:40:38 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Price] [int] NOT NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[InvoicePositions]  WITH CHECK ADD  CONSTRAINT [FK_InvoicePositions_Invoices] FOREIGN KEY([InvoiceId])
+REFERENCES [dbo].[Invoices] ([Id])
+GO
+ALTER TABLE [dbo].[InvoicePositions] CHECK CONSTRAINT [FK_InvoicePositions_Invoices]
+GO
+ALTER TABLE [dbo].[InvoicePositions]  WITH CHECK ADD  CONSTRAINT [FK_InvoicePositions_Products] FOREIGN KEY([ProductId])
+REFERENCES [dbo].[Products] ([Id])
+GO
+ALTER TABLE [dbo].[InvoicePositions] CHECK CONSTRAINT [FK_InvoicePositions_Products]
+GO
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD  CONSTRAINT [FK_Invoices_Clients] FOREIGN KEY([ClientNumber])
+REFERENCES [dbo].[Clients] ([Id])
+GO
+ALTER TABLE [dbo].[Invoices] CHECK CONSTRAINT [FK_Invoices_Clients]
+GO
---
+++ T6L13PracaDomowa/HomeworkQuery.sql
@@ -0,0 +1,26 @@
+﻿SELECT TOP (1000)[Number]
+      ,cli.FirstName
+	  ,cli.LastName
+  FROM [Invoices] inv
+  Left join Clients cli on cli.Id = inv.ClientNumber
+
+  SELECT TOP (1000) inv.Number
+      ,pro.Name
+      ,pro.Price
+  FROM [Enterprise].[dbo].[InvoicePositions] invpos
+  Left join Products pro on pro.Id = invpos.ProductId
+  Left join Invoices inv on inv.Id = invpos.InvoiceId
+  order by inv.Number
+
+SELECT TOP (1000) inv.Number
+      ,SUM(Quantity) as [ilość wszystkich produktów]
+  FROM [Enterprise].[dbo].[InvoicePositions] invpos
+      Left join Invoices inv on inv.Id = invpos.InvoiceId
+  group by inv.Number
+
+SELECT TOP (1000) inv.Number
+      ,SUM(pro.Price*invpos.Quantity) as [Łączna kwota]
+  FROM [Enterprise].[dbo].[InvoicePositions] invpos
+  Left join Products pro on pro.Id = invpos.ProductId
+  Left join Invoices inv on inv.Id = invpos.InvoiceId
+  group by inv.Number
