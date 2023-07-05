9cfa684659e5bdf8d2529a8f780146ac47a06e7b
Kammitora <kammitora@outlook.com>
2023-07-05 22:13:51 +0200

    first UI and MVVM Skeleton

---
+++ T7L29PracaDomowa/Models/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T7L29PracaDomowa.Models
+{
+    class Settings
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ T7L29PracaDomowa/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T7L29PracaDomowa.ViewModels
+{
+    class LoginViewModel
+    {
+        public LoginViewModel()
+        {
+
+        }
+    }
+}
